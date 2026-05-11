02bccdb9dee696c59b07895e533556f581165c64
DeMo900 <dodoadam893@gmail.com>
2026-05-11 15:02:48 +0300

    auth with delivery

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,35 @@
 import { Hono } from "hono";
+import { clientSignupSchema
+ ,deliverySignupSchema} from "../validation/auth";
+import { zValidator } from "@hono/zod-validator";
+import bcrypt from "bcryptjs"
 const authRouter = new Hono()
-
-authRouter.post("/signup/client")
-authRouter.post("/signup/delivery")
-authRouter.post("/login")
-
+.post("/signup/client",
+   zValidator("json",clientSignupSchema,(result,c)=>{
+    if(!result.success){
+        return c.json({error:result.error.issues[0].message},400)
+    }
+   }) 
+    ,async(c)=>{
+        try{
+    const {username,password,email,phoneNumber,address,activityType,activityName} =  c.req.valid("json")
+    const hashedPassword = await bcrypt.hash(password, 10)
+    return c.json({message:"User registered successfully",data:{username,email,phoneNumber,address,activityType,activityName,hashedPassword}},201)
+        }catch(error){
+            return c.json({error:"Internal server error"},500)
+        }
+    }).post("/signup/delivery",zValidator("json",deliverySignupSchema,(result,c)=>{
+        if(!result.success){
+            return c.json({error:result.error.issues[0].message},400)
+        }
+       }) 
+        ,async(c)=>{
+            try{
+        const {username,password,email,phoneNumber} =  c.req.valid("json")
+        const hashedPassword = await bcrypt.hash(password, 10)
+        return c.json({message:"User registered successfully",data:{username,email,phoneNumber,hashedPassword}},201)
+            }catch(error){
+                return c.json({error:"Internal server error"},500)
+            }
+        })
 export default authRouter 
---
+++ src/validation/auth.ts
@@ -0,0 +1,29 @@
+import {z} from "zod";
+
+const baseSchema = z.object({
+    username: z.string().min(1,"username is required").min(3,"user name must be at least 3 characters").max(30,"user name must be at most 30 characters"),
+    password: z.string().min(1,"password is required").min(8, 'Password must be at least 8 characters')
+    .regex(/[A-Z]/, ' password must contain at least one uppercase letter')
+    .regex(/[a-z]/, 'password must contain at least one lowercase letter')
+    .regex(/[0-9]/, 'password must contain at least one number'),
+    confirmPassword: z.string().min(1,"confirm password is required"),
+    email: z.email("invalid email address"),
+    phoneNumber: z.string().min(1, 'phone number is required').regex(/^01[0125][0-9]{8}$/, 'Invalid phone number')
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+})
+
+const clientSignupSchema = baseSchema.extend({
+  address:z.string()
+    .min(1,"address is required")
+    .min(10, 'Address too short')
+    .max(255, 'Address too long'),
+    activityType: z.enum(["Wedding hall","Restaurant","Cafe","Club"],
+        {message:"activity type must be one of the following: Wedding hall, Restaurant, Cafe, Club"}),
+    activityName: z.string().max(50,"activity name is too long").min(1,"activity name is required"),
+})
+const loginSchema = z.object({
+    identifier: z.string().min(1,"identifier is required")  ,
+    password: z.string().min(1,"password is required")
+})
+export {clientSignupSchema,baseSchema as deliverySignupSchema,loginSchema}
