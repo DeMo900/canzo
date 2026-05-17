62d2ea7812062b53bddda5057c726880151833a7
DeMo900 <dodoadam893@gmail.com>
2026-05-17 18:39:08 +0300

    handling changing order status canceled

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -71,5 +71,30 @@ const adminRouter = new Hono<{Bindings:Bindings,Variables:Variables}>()
         console.log(`error while getting orders ${error}`)
         return c.json({error:"Internal server error"},500)
     }
-}) 
+}).patch("/order/:id",async(c)=>{
+    try{
+      const {id} = await c.req.param()
+      const {status,image} = await c.req.json()
+      if(status === "Pending" || status === "Completed" || status === "Cancelled"){
+        if (status === "Cancelled"){
+     const [updateOrderResult,setEmptyResult] = await c.env.canzo.batch([
+        c.env.canzo.prepare("UPDATE orders SET status = ?1 , updated_at = datetime('now') WHERE id = ?2").bind(status,id),
+        c.env.canzo.prepare("UPDATE baskets SET is_full = 0 , order_id = NULL, updated_at = datetime('now') WHERE order_id = ?1").bind(id)
+     ])
+    if(updateOrderResult.meta.changes === 0 || setEmptyResult.meta.changes === 0){
+        return c.json({error:"Order not found"},404)
+    }
+    return c.json({message:"Order cancelled successfully"},200)
+    }
+    if(status === "Completed"){
+        
+    }
+      }else{
+        return c.json({error:"Invalid status"},400)
+      }
+    }catch(error){
+        console.log(`error while updating order status ${error}`)
+        return c.json({error:"Internal server error"},500)
+    }
+})  
 export default adminRouter
